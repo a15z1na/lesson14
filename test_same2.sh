dee2333fcce85ab1ac94a43dec25ce34c9db7e45
Norihiro Arai <a15z1na@aiit.ac.jp>
2017-06-08 00:57:34 +0900

    Check test_same2
    test_same test_same2

---
+++ test_same2.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+tmp=/tmp/$$
+
+ERROR_EXIT () {
+  echo "$1" >&2
+  rm -f $tmp-*
+  exit 1
+}
+#########
+# TEST1 #
+#########
+echo aaa bbb NG > $tmp-ans
+./same.sh aaa bbb > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+
+#########
+# TEST2 #
+#########
+echo "same2.sh requires 2 string inputs" > $tmp-ans
+./same2.sh 2> $tmp-err && ERROR_EXIT "TEST2-1"
+diff $tmp-ans $tmp-err || ERROR_EXIT "TEST2-2"
+
+echo OK
+rm -f $tmp-*
+exit 0
